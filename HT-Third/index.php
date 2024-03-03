ac6db6dd8de6bcbc933b343fee7f08ba68bb7cac
kuchinsk1y <98710097+kuchinsk1y@users.noreply.github.com>
2024-03-03 15:36:00 +0200

    Add files via upload

---
+++ HT-Third/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Third Lesson</title>
+</head>
+
+<body>
+    <?php
+
+    // Завдання на додавання чисел
+
+    $num1 = 15;
+    $num2 = 27;
+
+    // Задані числа
+    echo "Завдання: $num1 + $num2 <br>\n";
+
+    // Обчислення суми
+    $sum = $num1 + $num2;
+
+    // Виведення результату
+    echo "Відповідь: $sum\n";
+
+    ?>
+
+</body>
+
+</html>
